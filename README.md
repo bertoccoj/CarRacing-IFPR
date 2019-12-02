4f88beafad10dff95f8b40d02ca372c1fe9e9b25
bertoccoj <joaovitorbetocco51@gmail.com>
2019-12-02 03:30:32 -0300

    updatre run

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CarRacing-IFPR
 
-config do console:
-fonte: consolas
-altura: 35
-largura: 93
+config do console:<br>
+fonte: Fonte de varredura<br>
+
+execurat run.bat para compilar(por padrão compila sem debug)<br>
+para ativar debug basta rodar com o argumento 1: "run 1"
--- run.bat
+++ run.bat
@@ -1,2 +1,7 @@
-gcc scoreController.c menusController.c carracing.c display.c main.c -o game -D DEBUG=1 -D COLOR=0
+@echo off
+IF [%1]==[/?] SET /A debug = 0
+IF [%1]==[0] SET /A debug = 0
+IF [%1]==[1] SET /A debug = 1
+
+gcc scoreController.c menusController.c carracing.c display.c main.c -o game -D DEBUG=%debug%
 start .\game.exe
